87b682fd111d6613edf84048ef3c59f99497c0aa
vincaf <vincenzocafassog@gmail.com>
2022-06-17 16:40:00 +0200

    array and user mail variable initialized

---
+++ Mail/script.js
@@ -0,0 +1,6 @@
+// Inizializzazione array con mail approvate
+const mailApproved = [ "tizio@gmail.com", "caio@gmail.com", 'sempronio@gmail.com'];
+console.log(mailApproved);
+// Inizializzazione variabile di mail inserita dall'utente
+const userMail = prompt('inserisci una mail');
+console.log("Questa è la mail che hai inserito" + userMail);
