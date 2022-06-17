644b297fce1fcd00bff7de84937aed3143a53d37
vincaf <vincenzocafassog@gmail.com>
2022-06-17 16:46:55 +0200

    Initialized verification and message variable

--- Mail/script.js
+++ Mail/script.js
@@ -1,6 +1,13 @@
 // Inizializzazione array con mail approvate
 const mailApproved = [ "tizio@gmail.com", "caio@gmail.com", 'sempronio@gmail.com'];
 console.log(mailApproved);
+
 // Inizializzazione variabile di mail inserita dall'utente
 const userMail = prompt('inserisci una mail');
 console.log("Questa è la mail che hai inserito" + userMail);
+
+// Inizializzazione variabile di verifica e messaggio
+let mailVerified = false;
+let message;
+
+
