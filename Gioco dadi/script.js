6755adea551ba4fbf18df59cac669edafd899b5a
vincaf <vincenzocafassog@gmail.com>
2022-06-17 17:51:19 +0200

    Added conditions and printing of the result

--- Gioco dadi/script.js
+++ Gioco dadi/script.js
@@ -1,9 +1,27 @@
+// Traccia del sistema di gioco
 console.log("Verrà lanciato un dado per ogni giocatore, il numero più alto vince")
 
+// Dichiarazione variabili utente e CPU
 let userNumber;
 let computerNumber;
 
+// Calcolo casuale del numero restituito dal lancio del dado
 userNumber = Math.round(Math.random() * 6);
 console.log("Il tuo lancio ha valore: " + userNumber)
 computerNumber = Math.round(Math.random() * 6);
 console.log("Il lancio della CPU ha valore: " + computerNumber)
+
+// Dichiarazione variabile messaggio per indicare il risultato
+let message;
+
+// Condizione di comparazione tra i numeri risultati dal lancio
+if (userNumber > computerNumber){
+    message = "HAI VINTO!";
+} else if (userNumber == computerNumber){
+    message = "Pareggio";
+} else {
+    message = "Mi dispiace, HAI PERSO!";
+}
+
+// Stampa dell'esito della partita in console
+console.log(message);
